42f5092bd54296a65d8841e6ad0d17ddeae02b1a
Uzytkownik <uzytkownik@kursGIT.pl>
2020-07-26 12:25:09 +0200

    init

---
+++ src/main/java/pl/sda/jpaspring/CustomerController.java
@@ -0,0 +1,8 @@
+package pl.sda.jpaspring;
+
+import org.springframework.stereotype.Controller;
+
+@Controller //to miejsce bedzie reagować na requesty z zewnatrz
+public class CustomerController {
+
+}
---
+++ src/main/java/pl/sda/jpaspring/CustomerService.java
@@ -0,0 +1,8 @@
+package pl.sda.jpaspring;
+
+import org.springframework.stereotype.Service;
+
+@Service //service powoduje utworzenie jednej instacji tej klasy (SINGLETON)
+public class CustomerService {
+
+}
